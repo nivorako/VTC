2b883a8d49808388c548470aa5f4c888557190f4
nivorako <nivo.rakoto@yahoo.fr>
2025-09-08 16:11:41 +0200

    update apiversion in stripe instance

--- server/config/stripe.ts
+++ server/config/stripe.ts
@@ -1,19 +1,17 @@
 import Stripe from 'stripe';
 
-
 let stripe: Stripe;
 
 if (!process.env.STRIPE_SECRET_KEY) {
   console.warn('La variable d\'environnement STRIPE_SECRET_KEY n\'est pas définie. Les fonctionnalités de paiement seront désactivées.');
   // Crée une instance factice de Stripe pour éviter les erreurs lors de l'initialisation
   stripe = new Stripe('sk_test_dummy', {
-    apiVersion: '2023-10-16',
+    apiVersion: '2023-10-16' as Stripe.LatestApiVersion,
     typescript: true,
   });
 } else {
   stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2023-10-16',
-    typescript: true,
+    apiVersion: '2023-10-16' as Stripe.LatestApiVersion,
   });
 }
 
